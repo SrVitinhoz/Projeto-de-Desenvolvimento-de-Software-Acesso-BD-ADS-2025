fb55f5cbc1bf3e177d798a04932bd27f892e9b47
V1kator <Victor.34241361@gmail.com>
2025-04-26 17:43:00 -0300

    Estrutura Base do projeto com todas as dependências e configurado BD

---
+++ Gestao_Escolar/DbContext/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Gestao_Escolar.DbContext
+{
+    public class AppDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+
+    }
+}
---
+++ Gestao_Escolar/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using Gestao_Escolar.DbContext;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Confi conexão com o banco de dados
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+    .UseSnakeCaseNamingConvention() // Adiciona a convenção de nomenclatura snake_case
+    );
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
